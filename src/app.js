c74e65919073a2782f1a11e55c708c2e14f60d43
SalZeus <juanfeguto2@gmail.com>
2023-03-14 01:45:21 +0000

    final commit to upload bugfixes

--- src/app.js
+++ src/app.js
@@ -7,15 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-  var pronoun = ["the", "our"];
-  var adj = ["great", "big"];
-  var noun = ["jogger", "racoon"];
+  function rNG(min, max) {
+    return (int = Math.random() * (max - min) + min);
+  }
+  var pronoun = ["the", "our", "Heavy", "danger", "loli", "metal"];
+  var adj = ["great", "big", "gucci", "garbage", "thicc", "lemon", "selling"];
+  var noun = [
+    "jogger",
+    "racoon",
+    "icecream",
+    "jimmy",
+    "rama",
+    "games",
+    "jackpot"
+  ];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        console.log(`${pronoun[i]}${adj[j]}${noun[k]}`);
+        console.log(`https://www.${pronoun[i]}${adj[j]}${noun[k]}.com`);
       }
     }
   }
